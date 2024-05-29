53e735bbe8f5c666c68658ea2cf521ce085f5839
hbohlen <bohlenhayden@gmail.com>
2024-05-28 22:18:55 -0500

    Initial clean project setup for React Three Fiber game development

---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+// src/main.tsx
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
