0edfa8189b0cf73cb291c59752be5ff39cdbc27f
hbohlen <bohlenhayden@gmail.com>
2024-05-30 18:23:55 -0500

    refactor: Update camera position and rotation in MainScene

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import MainScene from './scenes/MainScene';
 const App: React.FC = () => {
   return (
     <div style={{ height: '100vh', width: '100vw' }}>
-      <Canvas>
+      <Canvas camera={{ position: [0, 5, 10], fov: 50 }}>
         <ambientLight intensity={0.5} />
         <pointLight position={[10, 10, 10]} />
         <MainScene />
--- src/scenes/MainScene.tsx
+++ src/scenes/MainScene.tsx
@@ -1,10 +1,20 @@
 import React, { useEffect, useState } from 'react';
+import { useThree } from '@react-three/fiber';
 import RenderSystem from '../ecs/systems/RenderSystem';
 import Entity from '../ecs/entities/Entity';
 import Position from '../ecs/components/Position';
 import MeshComponent from '../ecs/components/MeshComponent';
 
 const MainScene: React.FC = () => {
+  const { camera } = useThree();
+
+  useEffect(() => {
+    // Set camera position
+    camera.position.set(0, 5, 10);
+    // Set camera rotation to look down at the cube
+    camera.lookAt(0, 0, 0);
+  }, [camera]);
+
   const [entities, setEntities] = useState<Entity[]>([]);
 
   useEffect(() => {
